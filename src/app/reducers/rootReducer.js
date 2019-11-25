92bf9b6ccf441a416630fc7dcc0d500e8831b7ea
minhtam2048 <minhtam2048@gmail.com>
2019-11-25 21:46:03 +0700

    Added reduxForm to create blog

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as FormReducer } from 'redux-form';
 import test from '../../features/testarea/test';
 import blogReducer from '../../features/blog/blogReducer';
 
 const rootReducer = combineReducers({
+    form: FormReducer,
     test: test,
     blogs: blogReducer
 });
--- src/features/blog/BlogForm/BlogForm.jsx
+++ src/features/blog/BlogForm/BlogForm.jsx
@@ -1,27 +1,26 @@
 import React, { Component } from 'react';
-import { Segment, Form, Button } from 'semantic-ui-react';
+import { Segment, Form, Button, Grid, Header } from 'semantic-ui-react';
 import {connect} from 'react-redux';
 import {createBlog, updateBlog} from '../blogActions';
+import {reduxForm, Field} from 'redux-form';
+import {composeValidators, combineValidators, isRequired, hasLengthGreaterThan} from 'revalidate';
 import cuid from 'cuid';
+import TextInput from '../../../app/common/form/TextInput';
+import TextArea from '../../../app/common/form/TextArea';
+import SelectInput from '../../../app/common/form/SelectInput';
+import DateInput from '../../../app/common/form/DateInput';
 
 const mapStateToProps = (state, ownProps) => {
     const blogId = ownProps.match.params.id;
 
-    let blog = {
-        title: '',
-        date: '',
-        city: '',
-        address: '',
-        description: '',
-        postedBy: ''
-    }
+   let blog= {};
 
     if (blogId && state.blogs.length > 0) {
         blog = state.blogs.filter(blog => blog.id === blogId)[0];
     }
 
     return {
-        blog
+        initialValues: blog
     }
 }
 
@@ -30,102 +29,77 @@ const mapDispatchToProps = {
     updateBlog
 };
 
+const validate = combineValidators({
+    title: isRequired({message: 'The blog title is required'}),
+    category: isRequired({message: 'The category is required'}),
+    description: composeValidators(
+        isRequired({message: 'Please enter the description'}),
+        hasLengthGreaterThan(4)({message: 'Description need to be atleast 5 characters'})
+    )(),
+    city: isRequired('city'),
+    address: isRequired('address'),
+    date: isRequired('date')
+})
+
+const category = [
+    {key: 'foods', text: 'Foods', value: 'foods'},
+    {key: 'travel', text: 'Travel', value: 'travel'},
+    {key: 'culture', text: 'Culture', value: 'culture'},
+    {key: 'lowCostTrip', text: 'Low Cost Trip', value: 'lowCostTrip'},
+    {key: 'famousPlace', text: 'Famous Place', value: 'famousPlace'},
+]
+
 class BlogForm extends Component {
 
-    state = {...this.props.blog};
-
-    componentDidMount() {
-        if (this.props.selectedBlog !== null) {
-            this.setState({
-                ...this.props.selectedBlog
-            });
-        };
-    }
-
-    handleFormSubmit = evt => {
-        evt.preventDefault();
-        if (this.state.id) {
-            this.props.updateBlog(this.state);
-            this.props.history.push(`/blogs/${this.state.id}`)
+    onFormSubmit = values => {
+        console.log(values);
+        // evt.preventDefault();
+        if (this.props.initialValues.id) {
+            this.props.updateBlog(values);
+            this.props.history.push(`/blogs/${this.props.initialValues.id}`)
         } else {
             const newBlog = {
-                ...this.state,
+                ...values,
                 id: cuid(),
-                postPhotoURL: '/assets/user.png'
+                postPhotoURL: '/assets/user.png',
+                postedBy:'Bob'
             }
             this.props.createBlog(newBlog);
-            this.props.history.push(`/blogs`);
+            this.props.history.push(`/blogs/${newBlog.id}`);
         }
     }
 
-    
-    handleInputChange = ({target: {name, value}}) => {
-        this.setState({
-            [name]: value
-        });
-    };
+   
 
     render() {
-        const { title, date, city, address, postedBy, description } = this.state;
+        const { history, initialValues, invalid, submitting, pristine } = this.props;
+
         return (
-            <Segment>
-                <Form onSubmit={this.handleFormSubmit} autoComplete='off'>
-                    <Form.Field>
-                        <label> Post Title</label>
-                        <input
-                        name='title'
-                        onChange={this.handleInputChange} 
-                        value={title} 
-                        placeholder="Post Title" />
-                    </Form.Field>
-                    <Form.Field>
-                        <label> Trip Date</label>
-                        <input
-                         name='date'
-                         onChange={this.handleInputChange} 
-                         value={date}  
-                        type="date" placeholder="Trip Date" />
-                    </Form.Field>
-                    <Form.Field>
-                        <label> Description </label>
-                        <input
-                         name='description'
-                         onChange={this.handleInputChange} 
-                         value={description}
-                         type='text'  
-                        placeholder="The description" />
-                    </Form.Field>
-                    <Form.Field>
-                        <label> City </label>
-                        <input
-                         name='city'
-                         onChange={this.handleInputChange} 
-                         value={city}  
-                        placeholder="The place you took on vacation" />
-                    </Form.Field>
-                    <Form.Field>
-                        <label> Address </label>
-                        <input
-                         name='address'
-                         onChange={this.handleInputChange} 
-                         value={address}  
-                        placeholder="The address" />
-                    </Form.Field>
-                    
-                    <Form.Field>
-                        <label> Posted By</label>
-                        <input
-                         name='postedBy'
-                         onChange={this.handleInputChange} 
-                         value={postedBy}  
-                        placeholder="the one who create this post" />
-                    </Form.Field>
-                    <Button positive type="submit">Submit</Button>
-                    <Button onClick={this.props.history.goBack} type="button">Cancel</Button>
-                </Form>
+            <Grid>
+                <Grid.Column width={10}>
+                <Segment>
+                    <Header sub color='teal' content='Blog Details'/>
+                    <Form onSubmit={this.props.handleSubmit(this.onFormSubmit)} autoComplete='off'>
+                        <Field name="title" component={TextInput} placeholder="Blog title" />
+                        <Field name="category" component={SelectInput} options={category} placeholder="Blog category" />
+                        <Field name="description" component={TextArea} placeholder="What is this post about?" />
+                        
+                        <Header sub color='teal' content='Location Details' />
+                            <Field name="city" component={TextInput} rows={4} placeholder="Where you traveled to ?" />
+                            <Field name="address" component={TextInput} placeholder="The more explicit information" />
+                            <Field name="date" component={DateInput} dateFormat='dd LLL yyyy h:mm a' showTimeSelect timeFormat='HH:mm' placeholder="When did you go?" />
+                        <Button disabled={invalid || submitting || pristine} positive type="submit">Submit</Button>
+                        <Button onClick={
+                            initialValues.id ? () => history.push(`/blogs/${initialValues.id}`) 
+                                             : () => history.push(`blogs`)} 
+                                             type="button">Cancel</Button>
+                    </Form>
             </Segment>
+                </Grid.Column>
+            </Grid>
+            
         );
     };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(BlogForm);
+export default connect(mapStateToProps, mapDispatchToProps)(reduxForm({form: 'blogForm', validate})(BlogForm));
